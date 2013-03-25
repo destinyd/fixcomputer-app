3ecf09560e3632865f191fdbee9dcc7d8973e4db
destinyd <destinyd_war@163.com>
2013-03-25 20:45:50 +0800

    fix back bug

--- app/src/main/java/DD/Android/FixComputer/ui/ActivityFC.java
+++ app/src/main/java/DD/Android/FixComputer/ui/ActivityFC.java
@@ -20,7 +20,7 @@ public abstract class ActivityFC extends RoboSherlockActivity {
         switch (item.getItemId()) {
             case android.R.id.home:  // This is the home button in the top left corner of the screen.
                 // Dont call finish! Because activity could have been started by an outside activity and the home button would not operated as expected!
-                Intent homeIntent = new Intent(this, ActivityFC.class);
+                Intent homeIntent = new Intent(this, ActivityMain.class);
                 homeIntent.addFlags(FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(homeIntent);
                 return true;
